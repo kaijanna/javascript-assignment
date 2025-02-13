ba668c56e8f9c9c322f32ccf503d9507415f33b2
kaijanna <kaijanna@hotmail.com>
2025-02-13 01:00:18 +0100

    modified:   index.html 	modified:   movie-info.html 	modified:   script.js

--- script.js
+++ script.js
@@ -1,54 +1,16 @@
-const container = document.querySelector("#productContainer")
+const container = document.querySelector("#movieDetails")
 
 
-async function fetchAndCreateProducts() {
+async function fetchAndCreateProductsById() {
     try{
-        const response = await fetch ("https://v2.api.noroff.dev/square-eyes")
+        const response = await fetch ("https://v2.api.noroff.dev/square-eyes/<id>") // og her forstår jeg ikke hvordan man skal hente produkter med id, får bare feil når jeg setter den inn her
         const data = await response.json()
-        const products = data.data
-
-        products.forEach(product => {
-            const card = document.createElement("div")
-            const image = document.createElement("img")
-            const content = document.createElement("div")
-            const title = document.createElement("h2")
-            const price = document.createElement("p")
-            const genre = document.createElement("h3")
-            
-            
-
-            card.className = 'card'
-            image.className = 'card-image'
-            content.className = 'card-content'
-            title.className = 'card-title'
-            price.className = 'card-price'
-            genre.className = 'card-genre'
-            
-           
-
-            image.src = product.image.url
-            image.alt = product.image.alt
-            title.textContent = product.title
-            price.textContent = product.price
-            genre.textContent = product.genre
-            
-            
-
-            content.appendChild(title)
-            content.appendChild(genre)
-            content.appendChild(price)
-            card.appendChild(genre)
-            card.appendChild(image)
-            card.appendChild(content)
-
-            container.appendChild(card)
-
-
-        });
+        const products = data.data // skal det være id typ her, isteden for data? 
+        console.log(data)
         
     } catch (err) {
         console.error('Error fetching products:',err)
     }
     
 }
-fetchAndCreateProducts()
+fetchAndCreateProductsById()
